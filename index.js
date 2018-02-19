288a8f6c38d879c5e6a7db788572c9bbedcfc560
Eduardo Magalhaes <ed83isme@gmail.com>
2018-02-19 01:01:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var name = "Susan"
-var height = "74"
+var name = "Susan";
+var height = "70";
 
-if (name === "Susan") {
-    console.log("The name is correct")
+if (name === "Joe") {
+    console.log("The name is correct");
 } else  {
-    console.log("Expected the name to be Susan")
+    console.log("Expected the name to be Susan");
 }
 
 // Don't worry about this
